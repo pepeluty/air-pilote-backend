97da1eead007e76b68b37f5699b17bf380abf1a0
Jose Luis Ortega Cares <jlortegacares@gmail.com>
2026-06-26 22:36:27 -0400

    fix(db): add mikro-orm cli dependency and fix glob patterns

--- mikro-orm.config.ts
+++ mikro-orm.config.ts
@@ -20,8 +20,8 @@ export default defineConfig({
   dbName: process.env.PG_DB ?? 'air-pilote',
   user: process.env.PG_USER ?? 'postgres',
   password: process.env.PG_PASSWORD ?? 'postgres',
-  entities: ['./dist/contexts/**/*.entity.js'],
-  entitiesTs: ['./src/contexts/**/*.entity.ts'],
+  entities: ['./dist/contexts/**/*Entity.js'],
+  entitiesTs: ['./src/contexts/**/*Entity.ts'],
   namingStrategy: UnderscoreNamingStrategy,
   migrations: {
     path: './dist/migrations',
